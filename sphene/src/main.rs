68f830f997b9b11abe8f1113ae9f5bce9f927866
tumGER <git@tomger.eu>
2023-07-27 20:19:38 +0200

    Works 100%

--- sphene/src/main.rs
+++ sphene/src/main.rs
@@ -7,37 +7,85 @@ use dotenv::dotenv;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::model::prelude::Reaction;
 use serenity::prelude::*;
 use serenity::utils::MessageBuilder;
 
 struct Handler;
 
+const TWITTER_URL: &str = "https://twitter.com/";
+const FXTWITTER_URL: &str = "https://fxtwitter.com/";
+const REMOVE_REACTION: char = '❌';
+
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, context: Context, mut msg: Message) {
-        if msg.content.contains("https://twitter.com/") {
+    async fn message(&self, context: Context, msg: Message) {
+        if msg.content.contains(TWITTER_URL) {
             let response = MessageBuilder::new()
                 .mention(&msg.author)
                 .push(": ")
-                .push(msg.content.replace("https://twitter.com/", "https://fxtwitter.com/"))
+                .push(msg.content.replace(TWITTER_URL, FXTWITTER_URL))
                 .build();
 
-            if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+            let rsp_msg = msg.channel_id.say(&context.http, &response).await;
+            if let Err(why) = &rsp_msg {
                 println!("Error sending message: {:?}", why);
             }
 
-            if msg.content.find(" ").is_none() {
-                // Delete message
-                if let Err(why) = msg.delete(&context.http).await {
-                    println!("Error deleting message: {:?}", why);
-                }
-            } else {
-                // Suppress embed
-                if let Err(why) = msg.suppress_embeds(&context.http).await {
-                    println!("Error removing embed message: {:?}", why);
-                }
+            // Add reaction to the message
+            if let Err(why) = rsp_msg.unwrap().react(&context.http, REMOVE_REACTION).await {
+                println!("Error adding reaction: {:?}", why);
+            }
+
+            // Delete message
+            if let Err(why) = msg.delete(&context.http).await {
+                println!("Error deleting message: {:?}", why);
             }
         }
+    }
+
+    async fn reaction_add(&self, ctx: Context, add_reaction: Reaction) {
+        // If not the remove reaction return
+        if !add_reaction.emoji.unicode_eq(&REMOVE_REACTION.to_string()) {
+            return;
+        }
+
+        let msg = add_reaction.message(&ctx.http).await.unwrap();
+        // If the message is not from the bot return
+        if !msg.author.bot {
+            return;
+        }
+
+        // Get the user who added the reaction
+        let user = add_reaction.user_id.unwrap().to_user(&ctx.http).await.unwrap();
+        // If the user is the bot return
+        if user.bot {
+            return;
+        }
+
+        // If the user is the person mentioned in the message delete the message
+        if !msg.mentions.contains(&user) {
+            return;
+        }
+
+        if let Err(why) = msg.delete(&ctx.http).await {
+            println!("Error deleting message: {:?}", why);
+        }
+
+        // Deleted Message Response
+        let rsp_msg = msg.channel_id.say(&ctx.http, "💣 Deleted Message").await;
+        if let Err(why) = &rsp_msg {
+            println!("Error sending message: {:?}", why);
+        }
+
+        // Sleep for 5 seconds
+        tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+
+        // Delete the response message
+        if let Err(why) = rsp_msg.unwrap().delete(&ctx.http).await {
+            println!("Error deleting message: {:?}", why);
+        }
+
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
@@ -54,7 +102,9 @@ async fn main() {
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::GUILD_MESSAGE_REACTIONS
+        | GatewayIntents::DIRECT_MESSAGE_REACTIONS;
     let mut client =
         Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
 
